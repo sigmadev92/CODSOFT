a9234dcb99fd57c77307050090a52465e4a2aa50
sigmadev92 <devanshofficial2000@gmail.com>
2024-09-24 15:18:02 +0530

    folder structure of frontend made.

--- Level2/Task2_JobBoard/frontend/src/App.js
+++ Level2/Task2_JobBoard/frontend/src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
